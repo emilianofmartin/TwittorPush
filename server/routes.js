7629c02777dfa20bf90fda90a14c6543dcb1192b
Emiliano F. Martín <emiliano.martin@gmail.com>
2020-12-02 13:18:35 -0300

    #17

--- server/routes.js
+++ server/routes.js
@@ -158,7 +158,7 @@ router.post('/pushBookingWasConfirmed', (req, rsp) => {
 
     for(var i=0;i<recipients.length;i++) {
       err = "Trying";
-      err = await push.sendPushSubscription(post, recipients[i], p256[i], auth[i]);
+      err = push.sendPushSubscription(post, recipients[i], p256[i], auth[i]);
       post.recipients.push({
         recipient: recipients[i],
         error: err
