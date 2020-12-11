4d08623e4ae02d0ab7162f90a90c1fe26134e551
Emiliano F. Martín <emiliano.martin@gmail.com>
2020-12-11 10:44:48 -0300

    getSubscriptions

--- server/push.js
+++ server/push.js
@@ -111,7 +111,7 @@ module.exports.sendPushSubscription = (post, recipient, p256, auth) => {
 module.exports.getSubscriptions = () => {
     var subs = {};
     for(i=0;i<subscriptions.length;i++) {
-        //subs.push(subscriptions[i]);
+        subs.push(subscriptions[i]);
     }
 
     return subscriptions.length;
