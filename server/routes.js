9cd9c1ce529d41bc2f88cbfec97942486f28be7e
Emiliano F. Martín <emiliano.martin@gmail.com>
2020-12-15 14:23:36 -0300

    waitForIt(3000)

--- server/routes.js
+++ server/routes.js
@@ -480,7 +480,7 @@ router.get('/fullSubscription/:regId/:keyP256/:keyAuth', (req, rsp) => {
 
 
     push.sendPushSubscription(post, regId, keyP256, keyAuth);
-    waitForIt(1500);
+    waitForIt(3000);
 
     const subs = push.getSubscriptions();
     let includes = false;
