a556756fd7cd82f08633645d195c42a8722c879c
Emiliano F. Martín <emiliano.martin@gmail.com>
2020-12-21 14:32:37 -0300

    let a = i

--- server/push.js
+++ server/push.js
@@ -111,6 +111,14 @@ module.exports.sendPushSubscription = (post, recipient, p256, auth) => {
             }
         });
     sentNotifications.push(p);
+
+    let loops = 1000;
+    while(loops > 0) {
+        loops--;
+        for(var i = 0;i<100000 && error == "Trying"; i++) {
+            let a = i;
+        }
+    }
     
     Promise.all(sentNotifications)
         .then(() => {
